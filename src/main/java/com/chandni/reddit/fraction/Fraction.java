62e63977872a26cffb2c5c7ba5393d9eef6b1355
Chandni Patel <chandni_patel@gap.com>
2017-01-22 12:28:22 -0800

    Add package for Fraction logic

---
+++ src/main/java/com/chandni/reddit/fraction/Fraction.java
@@ -0,0 +1,7 @@
+package com.chandni.reddit.fraction;
+
+public class Fraction {
+    public Fraction simplify() {
+        return null;
+    }
+}
---
+++ src/main/java/com/chandni/reddit/fraction/Main.java
@@ -0,0 +1,10 @@
+package com.chandni.reddit.fraction;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Fraction fraction = new Fraction();
+        System.out.println(fraction.simplify().toString());
+    }
+}
