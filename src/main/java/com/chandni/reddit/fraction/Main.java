6464891aa0b4f3e345026f655391056cb920c9af
Chandni Patel <chandni_patel@gap.com>
2017-01-22 13:22:25 -0800

    small refactor, remove hello world sout

--- src/main/java/com/chandni/reddit/fraction/Main.java
+++ src/main/java/com/chandni/reddit/fraction/Main.java
@@ -2,8 +2,6 @@ package com.chandni.reddit.fraction;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
         Fraction fraction = new Fraction(4, 8);
         System.out.println(fraction.simplify().toString());
     }
