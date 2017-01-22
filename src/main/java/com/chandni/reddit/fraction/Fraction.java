d8addfc1239f0640c156c207b02245a9e4da0463
Chandni Patel <chandni_patel@gap.com>
2017-01-22 12:32:43 -0800

    Restructured fraction class

--- src/main/java/com/chandni/reddit/fraction/Fraction.java
+++ src/main/java/com/chandni/reddit/fraction/Fraction.java
@@ -1,6 +1,15 @@
 package com.chandni.reddit.fraction;
 
 public class Fraction {
+
+    private Integer numerator;
+    private Integer denominator;
+
+    public Fraction(Integer numerator, Integer denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
     public Fraction simplify() {
         return null;
     }
--- src/main/java/com/chandni/reddit/fraction/Main.java
+++ src/main/java/com/chandni/reddit/fraction/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        Fraction fraction = new Fraction();
+        Fraction fraction = new Fraction(4, 8);
         System.out.println(fraction.simplify().toString());
     }
 }
