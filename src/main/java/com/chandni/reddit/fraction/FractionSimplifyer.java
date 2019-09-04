b2f551aa5be0d49eb9f76ab451961a0dc15a025e
Chandni Patel <chandnir.patel@gmail.com>
2019-09-03 20:22:31 -0400

    attempt B

---
+++ src/main/java/com/chandni/reddit/fraction/FractionSimplifyer.java
@@ -0,0 +1,7 @@
+package com.chandni.reddit.fraction;
+
+public class FractionSimplifyer {
+    public static void go() {
+        System.out.println("Hello World");
+    }
+}
--- src/main/java/com/chandni/reddit/fraction/Main.java
+++ src/main/java/com/chandni/reddit/fraction/Main.java
@@ -2,6 +2,7 @@ package com.chandni.reddit.fraction;
 
 public class Main {
     public static void main(String[] args) {
+        FractionSimplifyer.go();
         Fraction fraction = new Fraction(4, 8);
         System.out.println(fraction.simplify().toString());
     }
